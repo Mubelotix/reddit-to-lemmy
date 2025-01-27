70fba0a6455dde9dd3d7f8a4f28a4607b10c80ce
Mubelotix <mubelotix@gmail.com>
2025-01-27 22:16:03 +0100

    Add wildard service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn proxy() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = std::env::var("PORT").map(|p| p.parse().expect("Port must be a number")).unwrap_or(3000);
+
+    let fut = HttpServer::new(|| {
+        App::new()
+            .default_service(web::route().to(proxy))
+    })
+    .bind(("127.0.0.1", port))?
+    .run();
+
+    println!("Server running at http://localhost:{port}");
+
+    fut.await
 }
