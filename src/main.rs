55f4d36afecc10be94aa0c6947288e04622b00eb
Mubelotix <mubelotix@gmail.com>
2025-01-28 01:35:53 +0000

    Fix loading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{web::{self, Bytes}, App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
-use awc::{error::{PayloadError, SendRequestError}, http::{uri::{InvalidUri, InvalidUriParts, Scheme}, Uri}};
+use awc::{error::{PayloadError, SendRequestError}, http::{uri::{InvalidUri, InvalidUriParts, Scheme}, Method, Uri}};
 use futures::StreamExt;
 
 const READABLE_BODIES: &[&str] = &[
@@ -7,6 +7,8 @@ const READABLE_BODIES: &[&str] = &[
     "application/json",
     "multipart/form-data",
     "text/plain",
+    "text/html",
+    "text/css",
 ];
 
 #[derive(Debug)]
@@ -21,6 +23,7 @@ enum ProxyError {
 
 impl std::fmt::Display for ProxyError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        println!("error");
         match self {
             ProxyError::RequestPayload(e) => write!(f, "Request payload error: {}", e),
             ProxyError::InvalidAuthority(e) => write!(f, "Invalid authority: {}", e),
@@ -35,40 +38,22 @@ impl std::fmt::Display for ProxyError {
 impl ResponseError for ProxyError {
 }
 
-async fn proxy(mut request: HttpRequest) -> Result<impl Responder, ProxyError> {
+async fn proxy(request: HttpRequest, mut payload: web::Payload) -> Result<impl Responder, ProxyError> {
     use ProxyError::*;
-
-    let body = match request.take_payload() {
-        actix_web::dev::Payload::None => Vec::new(),
-        actix_web::dev::Payload::Stream { payload: () } => Vec::new(),
-        actix_web::dev::Payload::H1 { mut payload } => {
-            let mut body = Vec::new();
-            while let Some(chunk) = payload.next().await {
-                body.extend_from_slice(&chunk.map_err(RequestPayload)?);
-            }
-            body
-        },
-        actix_web::dev::Payload::H2 { mut payload } => {
-            let mut body = Vec::new();
-            while let Some(chunk) = payload.next().await {
-                body.extend_from_slice(&chunk.map_err(RequestPayload)?);
-            }
-            body
-        },
-    };
+    
+    let mut body = Vec::new();
+    while let Some(item) = payload.next().await {
+        body.extend_from_slice(&item.map_err(RequestPayload)?);
+    }
 
     let readable_body = request.headers().get("content-type").map(|ct| ct.to_str().unwrap()).map(|ct| {
         READABLE_BODIES.iter().any(|rb| ct.starts_with(rb))
     }).unwrap_or(false);
 
-    match readable_body {
-        true => println!("{request:?}\n{:?}", String::from_utf8_lossy(&body)),
-        false => println!("{request:?}\n{body:?}"),
-    }
-
     let uri = request.uri();
     let path = uri.path().trim_start_matches('/');
     let (target_domain, target_path) = path.split_once('/').unwrap_or((path, ""));
+    let target_path = format!("/{}", target_path);
 
     let mut uri_parts = uri.clone().into_parts();
     uri_parts.scheme = Some(Scheme::HTTPS);
@@ -79,21 +64,50 @@ async fn proxy(mut request: HttpRequest) -> Result<impl Responder, ProxyError> {
     let client = awc::Client::default();
     let mut target_request = client.request_from(target_uri, request.head());
     target_request.headers_mut().remove("host");
-    let mut target_response = target_request.send_body(body).await.map_err(SendRequest)?;
+    target_request.headers_mut().remove("x-forwarded-for");
+    target_request.headers_mut().remove("x-forwarded-proto");
+    target_request.headers_mut().remove("x-forwarded-protocol");
+    target_request.headers_mut().remove("x-forwarded-port");
+    target_request.headers_mut().remove("x-forwarded-host");
+    target_request.headers_mut().remove("x-real-ip");
+    target_request.headers_mut().remove("cookie");
+    target_request.headers_mut().remove("connection");
+    
+    let log = match readable_body {
+        true => format!("{target_request:?}\n{:?}", String::from_utf8_lossy(&body)),
+        false => format!("{target_request:?}\nbinary"),
+    };
+
+    let mut target_response = match request.method() == Method::GET || body.is_empty() {
+        true => target_request.send().await.map_err(SendRequest)?,
+        false => target_request.send_body(body).await.map_err(SendRequest)?,
+    };
     let response_body = target_response.body().await.map_err(ResponsePayload)?;
 
-    let readable_body = target_response.headers().get("content-type").map(|ct| ct.to_str().unwrap()).map(|ct| {
-        READABLE_BODIES.iter().any(|rb| ct.starts_with(rb))
-    }).unwrap_or(false);
 
-    match readable_body {
-        true => println!("{target_response:?}\n{:?}", String::from_utf8_lossy(&response_body)),
-        false => println!("{target_response:?}\n{response_body:?}"),
+    if target_response.status().is_client_error() || target_response.status().is_server_error() {
+        println!("{log}");
+        
+        let readable_body = target_response.headers().get("content-type").map(|ct| ct.to_str().unwrap()).map(|ct| {
+            READABLE_BODIES.iter().any(|rb| ct.starts_with(rb))
+        }).unwrap_or(false);
+    
+        match readable_body {
+            true => println!("{target_response:?}\n{:?}", String::from_utf8_lossy(&response_body)),
+            false => println!("{target_response:?}\nbinary data"),
+        }
     }
 
     let mut response_builder = HttpResponse::new(target_response.status());
     *response_builder.headers_mut() = target_response.headers().clone();
-    Ok(response_builder.set_body(response_body))
+    response_builder.headers_mut().append("content-length".try_into().unwrap(), response_body.len().to_string().parse().unwrap());
+    response_builder.headers_mut().remove("transfer-encoding");
+    response_builder.headers_mut().remove("content-encoding");
+    let response = response_builder.set_body(response_body);
+
+    println!("{response:?}");
+
+    Ok(response)
 }
 
 #[actix_web::main]
