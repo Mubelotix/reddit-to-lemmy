b1db4f606bed18c116e5b5882dfeb6f6a8a1fdf5
Mubelotix <mubelotix@gmail.com>
2025-01-30 15:23:45 +0000

    Break main into 2 programs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 #![recursion_limit = "512"]
 
-use actix_web::{error::ErrorUnauthorized, guard::{Guard, GuardContext}, post, web::{self, Bytes}, App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError};
-use awc::{error::{PayloadError, SendRequestError}, http::{uri::{InvalidUri, InvalidUriParts, Scheme}, Method, Uri}};
+use actix_web::{guard::{Guard, GuardContext}, web, App, HttpRequest, HttpResponse, HttpServer};
 use base64::Engine;
-use futures::StreamExt;
-use lemmy_client::lemmy_api_common::{lemmy_db_schema::{source::{community::Community, post::Post}, CommentSortType, SortType}, lemmy_db_views::structs::PostView};
+use lemmy_client::lemmy_api_common::lemmy_db_schema::{source::{community::Community, post::Post}, CommentSortType, SortType};
 use log::{info, warn};
 use serde::{Deserialize, Deserializer};
 
@@ -154,128 +152,6 @@ pub fn get_jwt(req: &HttpRequest) -> Option<String> {
     Some(jwt)
 }
 
-const READABLE_BODIES: &[&str] = &[
-    "application/x-www-form-urlencoded",
-    "application/json",
-    "multipart/form-data",
-    "text/plain",
-    "text/html",
-    "text/css",
-];
-
-#[derive(Debug)]
-enum ProxyError {
-    RequestPayload(PayloadError),
-    InvalidAuthority(InvalidUri),
-    InvalidQuery(InvalidUri),
-    InvalidUriParts(InvalidUriParts),
-    SendRequest(SendRequestError),
-    ResponsePayload(PayloadError),
-}
-
-impl std::fmt::Display for ProxyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            ProxyError::RequestPayload(e) => write!(f, "Request payload error: {}", e),
-            ProxyError::InvalidAuthority(e) => write!(f, "Invalid authority: {}", e),
-            ProxyError::InvalidQuery(e) => write!(f, "Invalid query: {}", e),
-            ProxyError::InvalidUriParts(e) => write!(f, "Invalid URI parts: {}", e),
-            ProxyError::SendRequest(e) => write!(f, "Send request error: {}", e),
-            ProxyError::ResponsePayload(e) => write!(f, "Response payload error: {}", e),
-        }
-    }
-}
-
-impl ResponseError for ProxyError {
-    fn status_code(&self) -> awc::http::StatusCode {
-        eprintln!("{self}");
-
-        awc::http::StatusCode::INTERNAL_SERVER_ERROR
-    }
-}
-
-async fn proxy(request: HttpRequest, mut payload: web::Payload) -> Result<impl Responder, ProxyError> {
-    use ProxyError::*;
-
-    const WANTED_OPERATIONS: &[&str] = &["CommentsPageAdPost", "CommentTreeAds", "GetRedditGoldBalance", "GetRedditGoldAllTimeBalance", "EmailPermission", "UserComments", "UserSubmittedPostSets", "DiscoverBarRecommendations", "GetMatrixChatUsersByIds", "GetPrivateMessages", "GetInboxNotificationFeed", "GetNotificationSettingsLayoutByChannel", "TrendingSearches", "PopularFeedSdui"];
-    
-    let mut body = Vec::new();
-    while let Some(item) = payload.next().await {
-        body.extend_from_slice(&item.map_err(RequestPayload)?);
-    }
-
-    let request_body_readable = request.headers().get("content-type").map(|ct| ct.to_str().unwrap()).map(|ct| {
-        READABLE_BODIES.iter().any(|rb| ct.starts_with(rb))
-    }).unwrap_or(false);
-
-    let uri = request.uri();
-    let path = uri.path().trim_start_matches('/');
-    let (target_domain, target_path) = path.split_once('/').unwrap_or((path, ""));
-    let target_path = format!("/{}", target_path);
-
-    let mut uri_parts = uri.clone().into_parts();
-    uri_parts.scheme = Some(Scheme::HTTPS);
-    uri_parts.authority = Some(target_domain.parse().map_err(InvalidAuthority)?);
-    uri_parts.path_and_query = Some(target_path.parse().map_err(InvalidQuery)?);
-    let target_uri = Uri::from_parts(uri_parts).map_err(InvalidUriParts)?;
-
-    let client = awc::Client::default();
-    let mut target_request = client.request_from(target_uri, request.head());
-    target_request.headers_mut().remove("host");
-    target_request.headers_mut().remove("x-forwarded-for");
-    target_request.headers_mut().remove("x-forwarded-proto");
-    target_request.headers_mut().remove("x-forwarded-protocol");
-    target_request.headers_mut().remove("x-forwarded-port");
-    target_request.headers_mut().remove("x-forwarded-host");
-    target_request.headers_mut().remove("x-real-ip");
-    target_request.headers_mut().remove("connection");
-    
-    let log_part1 = match request_body_readable {
-        _ if body.is_empty() => format!("{target_request:?}\nno content"),
-        true => format!("{target_request:?}\n{}", String::from_utf8_lossy(&body)),
-        false => format!("{target_request:?}\nbinary"),
-    };
-
-    let mut target_response = match request.method() == Method::GET || body.is_empty() {
-        true => target_request.send().await.map_err(SendRequest)?,
-        false => target_request.send_body(body).await.map_err(SendRequest)?,
-    }; 
-    let response_body = target_response.body().await.map_err(ResponsePayload)?;
-
-    let response_body_readable = target_response.headers().get("content-type").map(|ct| ct.to_str().unwrap()).map(|ct| {
-        READABLE_BODIES.iter().any(|rb| ct.starts_with(rb))
-    }).unwrap_or(false);    
-
-    let mut response_builder = HttpResponse::new(target_response.status());
-    *response_builder.headers_mut() = target_response.headers().clone();
-    response_builder.headers_mut().insert("content-length".try_into().unwrap(), response_body.len().to_string().parse().unwrap());
-    response_builder.headers_mut().remove("transfer-encoding");
-    response_builder.headers_mut().remove("content-encoding");
-    // response_builder.headers_mut().remove("x-reddit-loid");
-    // if let Some(old_cookies) = response_builder.headers_mut().remove("set-cookie").map(|c| c.to_str().unwrap().to_string()).next() {
-    //     let new_cookies = old_cookies.replace("reddit.com;", "3000.code.mub.lol;");
-    //     response_builder.headers_mut().insert("set-cookie".try_into().unwrap(), new_cookies.parse().unwrap());
-    // }
-    
-    let response = response_builder.set_body(response_body.clone());
-
-    let log_part2 = match response_body_readable {
-        _ if response_body.is_empty() => format!("{response:?}\nno content"),
-        true => format!("{response:?}\n{}", String::from_utf8_lossy(&response_body)),
-        false => format!("{response:?}\nbinary data"),
-    };
-
-    #[allow(clippy::const_is_empty)]
-    if target_domain != "matrix.redditspace.com" {
-        let appolo_operation = request.headers().get("x-apollo-operation-name").and_then(|o| o.to_str().ok()).unwrap_or("unknown");
-        if WANTED_OPERATIONS.is_empty() || WANTED_OPERATIONS.contains(&appolo_operation) {
-            println!("{log_part1}\n{log_part2}");
-        }
-    }
-
-    Ok(response)
-}
-
 struct Apollo(&'static str);
 
 impl Guard for Apollo {
@@ -329,9 +205,12 @@ async fn main() -> std::io::Result<()> {
             .route("/gql-fed.reddit.com/", web::to(|req: HttpRequest| async move {
                     let operation_name = req.headers().get("x-apollo-operation-name").map(|o| o.to_str().unwrap()).unwrap_or("unknown");
                     warn!("Unknown Apollo operation: {operation_name}");
-                    HttpResponse::Forbidden().finish()
+                    HttpResponse::InternalServerError().finish()
             }))
-            .default_service(web::route().to(proxy))
+            .default_service(web::to(|req: HttpRequest| async move {
+                warn!("Unhandled request: {req:?}");
+                HttpResponse::InternalServerError().finish()
+            }))
     })
     .bind(("127.0.0.1", port))?
     .run();
